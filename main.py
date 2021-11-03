10eba73904a67c35d55fab44b1de231139d8e376
Pavel <sizykh86@gmail.com>
2021-11-03 18:30:09 +0000

    commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from aiohttp import web
 
 routes = web.RouteTableDef()
-
 @routes.get('/')
 async def hello(request):
-    return web.Response(text=request.headers['X-Real-IP'])
+    ip_address = request.headers['X-Real-IP']
+    html_response = f'<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">{ip_address}</pre></body></html>'
+
+    return web.Response(text=html_response, content_type='text/html')
 
 app = web.Application()
 app.add_routes(routes)
