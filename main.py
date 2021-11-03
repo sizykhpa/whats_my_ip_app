53021da0945ba7cb86f0b82e47c185e8edf905ed
Pavel <sizykh86@gmail.com>
2021-11-03 18:56:27 +0000

    commit

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ routes = web.RouteTableDef()
 @routes.get('/')
 async def hello(request):
     ip_address = request.headers['X-Real-IP']
-    html_response = f'<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">{ip_address}</pre></body></html>'
 
-    return web.Response(text=html_response, content_type='text/html')
+    return web.Response(text=ip_address)
 
 app = web.Application()
 app.add_routes(routes)
